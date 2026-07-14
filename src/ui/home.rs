563405d4d8f08be4e7ff333cbb7173099f2070f9
Sanju Sabu <sanjusabu.dev@gmail.com>
2026-07-14 11:50:29 +0530

    design(ui): Redesign home banner layout

--- src/ui/home.rs
+++ src/ui/home.rs
@@ -3,9 +3,9 @@
 use crate::app::App;
 use ratatui::{
     Frame,
-    layout::{Constraint, Flex, Layout, Rect},
+    layout::{Constraint, Flex, Layout, Margin, Rect},
     style::{Color, Modifier, Style},
-    widgets::Paragraph,
+    widgets::{Block, BorderType, Borders, Paragraph},
 };
 
 const BANNER: &str = include_str!("../../assets/banner.txt");
@@ -17,45 +17,93 @@ pub const MENU_ITEMS: [&str; 5] = [
     "Exit",
 ];
 
+// 1
+// The draw function that calls both banner and content draw functions (this is the main draw function)
 pub fn draw(frame: &mut Frame, area: Rect, state: &App) {
+    let [banner_area, content_area] =
+        Layout::vertical([Constraint::Length(12), Constraint::Min(0)]).areas(area);
+
+    draw_banner(frame, banner_area);
+    draw_content(frame, content_area, state);
+}
+
+// 2
+fn draw_banner(frame: &mut Frame, area: Rect) {
     let banner_width = BANNER
         .lines()
         .map(|line| line.chars().count())
         .max()
         .unwrap_or(0) as u16;
 
-    let [_, top_area, menu_area] = Layout::vertical([
-        Constraint::Length(3),
-        Constraint::Length(10),
-        Constraint::Min(0),
-    ])
-    .areas(area);
+    let banner_area = area.inner(Margin {
+        horizontal: 1,
+        vertical: 0,
+    });
 
-    let [banner_area] = Layout::horizontal([Constraint::Length(banner_width)])
+    let banner_block = Block::bordered()
+        .border_type(BorderType::Thick)
+        .border_style(Style::default().fg(Color::LightBlue));
+
+    frame.render_widget(banner_block.clone(), banner_area);
+
+    let inner_banner = banner_block.inner(banner_area).inner(Margin {
+        horizontal: 0,
+        vertical: 1,
+    });
+    let [banner_text_area] = Layout::horizontal([Constraint::Length(banner_width)])
         .flex(Flex::Center)
-        .areas(top_area);
+        .areas(inner_banner);
 
-    // Banner
     frame.render_widget(
-        Paragraph::new(BANNER).style(Style::default().fg(ratatui::style::Color::White)),
-        banner_area,
+        Paragraph::new(BANNER).style(Style::default().fg(Color::White)),
+        banner_text_area,
+    );
+}
+
+// 3
+fn draw_content(frame: &mut Frame, area: Rect, state: &App) {
+    let [line_area, content_area] =
+        Layout::vertical([Constraint::Length(1), Constraint::Min(0)]).areas(area);
+
+    frame.render_widget(
+        Block::bordered()
+            .border_type(BorderType::Thick)
+            .borders(Borders::TOP)
+            .border_style(Style::default().fg(Color::LightBlue)),
+        line_area,
     );
 
-    let [_, menu_area] =
-        Layout::vertical([Constraint::Length(3), Constraint::Min(0)]).areas(menu_area);
+    let [menu_area, divider_area, preview_area] = Layout::horizontal([
+        Constraint::Length(24),
+        Constraint::Length(1),
+        Constraint::Min(0),
+    ])
+    .areas(content_area);
+
+    frame.render_widget(
+        Block::new()
+            .borders(Borders::LEFT)
+            .border_style(Style::default().fg(Color::LightBlue)),
+        divider_area,
+    );
+
+    let menu_area = menu_area.inner(Margin {
+        horizontal: 3,
+        vertical: 1,
+    });
 
     let [quick, saved, dependencies, about, exit] = Layout::vertical([
-        Constraint::Length(3),
-        Constraint::Length(3),
-        Constraint::Length(3),
-        Constraint::Length(3),
-        Constraint::Length(3),
+        Constraint::Length(2),
+        Constraint::Length(2),
+        Constraint::Length(2),
+        Constraint::Length(2),
+        Constraint::Length(2),
     ])
     .areas(menu_area);
 
     if state.selected == 0 {
         frame.render_widget(
-            Paragraph::new("Quick Connect").centered().style(
+            Paragraph::new("QUICK CONNECT").style(
                 Style::default()
                     .fg(Color::LightBlue)
                     .add_modifier(Modifier::BOLD),
@@ -64,20 +112,18 @@ pub fn draw(frame: &mut Frame, area: Rect, state: &App) {
         );
     } else {
         frame.render_widget(
-            Paragraph::new("Quick Connect")
-                .style(
-                    Style::default()
-                        .fg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                )
-                .centered(),
+            Paragraph::new("QUICK CONNECT").style(
+                Style::default()
+                    .fg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            ),
             quick,
         );
     }
 
     if state.selected == 1 {
         frame.render_widget(
-            Paragraph::new("Saved Hosts").centered().style(
+            Paragraph::new("SAVED HOSTS").style(
                 Style::default()
                     .fg(Color::LightBlue)
                     .add_modifier(Modifier::BOLD),
@@ -86,19 +132,17 @@ pub fn draw(frame: &mut Frame, area: Rect, state: &App) {
         );
     } else {
         frame.render_widget(
-            Paragraph::new("Saved Hosts")
-                .style(
-                    Style::default()
-                        .fg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                )
-                .centered(),
+            Paragraph::new("SAVED HOSTS").style(
+                Style::default()
+                    .fg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            ),
             saved,
         );
     }
     if state.selected == 2 {
         frame.render_widget(
-            Paragraph::new("Dependencies").centered().style(
+            Paragraph::new("DEPENDENCIES").style(
                 Style::default()
                     .fg(Color::LightBlue)
                     .add_modifier(Modifier::BOLD),
@@ -107,20 +151,18 @@ pub fn draw(frame: &mut Frame, area: Rect, state: &App) {
         );
     } else {
         frame.render_widget(
-            Paragraph::new("Dependencies")
-                .style(
-                    Style::default()
-                        .fg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                )
-                .centered(),
+            Paragraph::new("DEPENDENCIES").style(
+                Style::default()
+                    .fg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            ),
             dependencies,
         );
     }
 
     if state.selected == 3 {
         frame.render_widget(
-            Paragraph::new("About").centered().style(
+            Paragraph::new("ABOUT").style(
                 Style::default()
                     .fg(Color::LightBlue)
                     .add_modifier(Modifier::BOLD),
@@ -129,20 +171,18 @@ pub fn draw(frame: &mut Frame, area: Rect, state: &App) {
         );
     } else {
         frame.render_widget(
-            Paragraph::new("About")
-                .style(
-                    Style::default()
-                        .fg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                )
-                .centered(),
+            Paragraph::new("ABOUT").style(
+                Style::default()
+                    .fg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            ),
             about,
         );
     }
 
     if state.selected == 4 {
         frame.render_widget(
-            Paragraph::new("Exit").centered().style(
+            Paragraph::new("EXIT").style(
                 Style::default()
                     .fg(Color::LightBlue)
                     .add_modifier(Modifier::BOLD),
@@ -151,13 +191,11 @@ pub fn draw(frame: &mut Frame, area: Rect, state: &App) {
         );
     } else {
         frame.render_widget(
-            Paragraph::new("Exit")
-                .style(
-                    Style::default()
-                        .fg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                )
-                .centered(),
+            Paragraph::new("EXIT").style(
+                Style::default()
+                    .fg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            ),
             exit,
         );
     }
