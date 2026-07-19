b900e4045d67caeaea57ae220ccb562456ec4826
Sanju Sabu <sanjusabu.dev@gmail.com>
2026-07-19 11:26:30 +0530

    refactor: apply clippy suggestions

--- src/ui/panes/pane2/about.rs
+++ src/ui/panes/pane2/about.rs
@@ -61,7 +61,7 @@ pub fn draw(frame: &mut Frame, inner: Rect, state: &State) {
         );
     }
 
-    if state.in_pane2 == true {
+    if state.in_pane2 {
         if hovered == Some(2) {
             frame.render_widget(
                 Paragraph::new("❯ BACK").style(
--- src/ui/panes/pane2/dependencies.rs
+++ src/ui/panes/pane2/dependencies.rs
@@ -82,7 +82,7 @@ pub fn draw(frame: &mut Frame, inner: Rect, state: &State) {
         );
     }
 
-    if state.in_pane2 == true {
+    if state.in_pane2 {
         if hovered == Some(3) {
             frame.render_widget(
                 Paragraph::new("❯ BACK").style(
